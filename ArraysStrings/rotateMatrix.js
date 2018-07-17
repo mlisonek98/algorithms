5065a6ff0c6ea826d6d8ef57ffff542191419efb
Marko Lisonek <mlisonek98@gmail.com>
2018-07-17 17:23:56 -0400

    Cracking the Coding Interview: 1.7

---
+++ ArraysStrings/rotateMatrix.js
@@ -0,0 +1,12 @@
+const rotateMtrix = function(matrix) {
+   matrix = matrix.reverse();
+
+   for (var i = 0; i < matrix.length; i++) {
+     for (var j = 0; j < i; j++) {
+       var temp = matrix[i][j];
+       matrix[i][j] = matrix[j][i];
+       matrix[j][i] = temp;
+     }
+   }
+   return matrix
+ };
